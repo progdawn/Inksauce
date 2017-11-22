e5e8dcbe86e0e7677aa76c577d2f384906e8fb99
Dawn Myers <progdawn@gmail.com>
2017-11-22 13:18:40 -0600

    Removed image sorting
    Removed sorting on images in Brands and Abilities

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 $('.carousel').carousel({
-  interval: 2000
+    interval: 2000
 })
 
 $(document).ready(function(){
     $('#brandstable').DataTable({
         responsive: true,
-        "paging":   false
+        "paging":   false,
+        "columnDefs": [ {
+            "targets": 'nosort',
+            "orderable": false
+        } ]
     });
 });
 
 $(document).ready(function(){
     $('#abilitiestable').DataTable({
         responsive: true,
-        "paging":   false
+        "paging":   false,
+        "columnDefs": [ {
+            "targets": 'nosort',
+            "orderable": false
+        } ]
     });
 });
 
