46808ff66983d7c9a022673c4286d808303e97a1
Dawn Myers <progdawn@gmail.com>
2017-11-21 22:16:14 -0600

    DATATABLES!
    Used datatables plugin for brands and abilities

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $('.carousel').carousel({
   interval: 2000
 })
+
+$(document).ready(function(){
+    $('#brandstable').DataTable({
+        responsive: true,
+        "paging":   false
+    });
+});
+
+$(document).ready(function(){
+    $('#abilitiestable').DataTable({
+        responsive: true,
+        "paging":   false
+    });
+});
+
