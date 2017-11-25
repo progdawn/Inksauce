cacab9f3a3100e44248440dff084658656e7d288
Dawn Myers <progdawn@gmail.com>
2017-11-24 20:57:45 -0600

    Weapons Display Works
    Dear God

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function (){
         } ]
     });
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        $($.fn.dataTable.tables(true)).DataTable()
+        $($.fn.dataTable.tables({visible: true, api: true} )).DataTable()
             .columns.adjust()
             .responsive.recalc();
     }); 
