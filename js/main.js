dacb3802f2c66eaf2d2c188fb239f821682c9e71
Dawn Myers <progdawn@gmail.com>
2017-12-03 14:48:12 -0600

    Working with placeholders
    Bleh. Next it's time to try real images

--- js/main.js
+++ js/main.js
@@ -65,7 +65,6 @@ $(document).ready( function () {
         tabbedTables.search( this.value ).draw();   
     });
 } );
-
 // Masonry stuff
 var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
@@ -76,93 +75,3 @@ var $grid = $('.grid').masonry({
 $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
 });
-/*$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});*/
-
-
-
-/*$(function () {
-
-    var $container = $('.grid-sizer').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 200,
-        percentPosition: true
-    });
-
-    // reveal initial images
-    $container.masonryImagesReveal($('.grid').find('.item'));
-});
-
-$.fn.masonryImagesReveal = function ($items) {
-    var msnry = this.data('masonry');
-    var itemSelector = msnry.options.itemSelector;
-    // hide by default
-    $items.hide();
-    // append to container
-    this.append($items);
-    $items.imagesLoaded().progress(function (imgLoad, image) {
-        // get item
-        // image is imagesLoaded class, not <img>, <img> is image.img
-        var $item = $(image.img).parents(itemSelector);
-        // un-hide item
-        $item.show();
-        // masonry does its thing
-        msnry.appended($item);
-    });
-
-    return this;
-};*/
-//Handle resizing Gallery pictures
-/*(function ($) {
-    var $container = $('.grid'),
-        colWidth = function () {
-            var w = $container.width(), 
-                columnNum = 1,
-                columnWidth = 0;
-            if (w > 1200) {
-                columnNum  = 5;
-            } else if (w > 900) {
-                columnNum  = 4;
-            } else if (w > 600) {
-                columnNum  = 3;
-            } else if (w > 300) {
-                columnNum  = 2;
-            }
-            columnWidth = Math.floor(w/columnNum);
-            $container.find('.item').each(function() {
-                var $item = $(this),
-                    multiplier_w = $item.attr('class').match(/item-w(\d)/),
-                    multiplier_h = $item.attr('class').match(/item-h(\d)/),
-                    width = multiplier_w ? columnWidth*multiplier_w[1]-4 : columnWidth-4,
-                    height = multiplier_h ? columnWidth*multiplier_h[1]*0.5-4 : columnWidth*0.5-4;
-                $item.css({
-                    width: width,
-                    height: height
-                });
-            });
-            return columnWidth;
-        },
-        isotope = function () {
-            $container.isotope({
-                itemSelector: '.grid-item',
-                masonry: {
-                    columnWidth: colWidth()
-                }
-            });
-        };
-    isotope();
-    $(window).on('debouncedresize', isotope);
-}(jQuery));*/
-/*$(document).ready( function() {
-
-  $('.grid').isotope({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-      columnWidth: '.grid-sizer'
-    }
-  });
-});*/
