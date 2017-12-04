23b509612ae8e9f2c796a22584c678ec2e777d66
Dawn Myers <progdawn@gmail.com>
2017-12-03 20:48:30 -0600

    Infinite scroll fix
    Maybe? I uploaded to my site's server and it worked great on my phone

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ $(document).ready(function (){
         $($.fn.dataTable.tables({visible: true, api: true, "deferRender": true} )).DataTable()
             .columns.adjust()
             .responsive.recalc();
-        
+
     }); 
     $('.nosort').removeClass('sorting_asc');
 });
@@ -94,7 +94,7 @@ $(document).ready( function () {
 //---- Gallery ----
 
 
-var $grid = $('.grid').masonry({
+/*var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     percentPosition: true
@@ -102,4 +102,64 @@ var $grid = $('.grid').masonry({
 //Layout Masonry after each image loads
 $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
-});
+});*/
+
+(function() {
+
+    // Main content container
+    var $container = $('.grid');
+
+    // Masonry + ImagesLoaded
+    $container.imagesLoaded(function(){
+        $container.masonry({
+            // selector for entry content
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
+    });
+
+    // Infinite Scroll
+    $container.infinitescroll({
+
+        // selector for the paged navigation (it will be hidden)
+        navSelector  : ".navigation",
+        // selector for the NEXT link (to page 2)
+        nextSelector : ".nav-previous a",
+        // selector for all items you'll retrieve
+        itemSelector : ".grid-item",
+
+        // finished message
+        loading: {
+            finishedMsg: 'No more pages to load.'
+        }
+    },
+
+                              // Trigger Masonry as a callback
+                              function( newElements ) {
+        // hide new items while they are loading
+        var $newElems = $( newElements ).css({ opacity: 0 });
+        // ensure that images load before adding to masonry layout
+        $newElems.imagesLoaded(function(){
+            // show elems now they're ready
+            $newElems.animate({ opacity: 1 });
+            $container.masonry( 'appended', $newElems, true );
+        });
+
+    });
+
+    /**
+	 * OPTIONAL!
+	 * Load new pages by clicking a link
+	 */
+
+    // Pause Infinite Scroll
+    $(window).unbind('.infscr');
+
+    // Resume Infinite Scroll
+    $('.nav-previous a').click(function(){
+        $container.infinitescroll('retrieve');
+        return false;
+    });
+
+})();
